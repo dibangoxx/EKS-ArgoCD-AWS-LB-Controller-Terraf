ef73b1e6679307d36af90324cfd20d1cc0641167
Emmanuel Madu <99225889+dibangoxx@users.noreply.github.com>
2024-09-03 10:23:07 -0700

    Update backend.tf

--- vpc-ec2/backend.tf
+++ vpc-ec2/backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.9.5"
+  required_version = "~> 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,7 +11,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "fueltrax-buckt"
     region         = "us-east-1"
     key            = "vpc/terraform.tfstate"
     dynamodb_table = "Lock-Files"
